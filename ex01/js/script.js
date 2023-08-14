f82357782ee84c927d2acccd90deda4cfa2b047a
Gabriel.s <gabrielsantosayres@gmail.com>
2023-08-14 17:52:21 -0300

    aprendendo js

---
+++ ex01/js/script.js
@@ -0,0 +1,8 @@
+let num1 = prompt(`Digite um numero`);
+let num2 = prompt('Digite outro  número');
+
+num1 = num1;
+num2 = Number(num2);
+
+alert(`O resultado da sua conta foi: ${ typeof(num1)
+}` );
---
+++ ex02/js/script.js
@@ -0,0 +1,7 @@
+let varA = 'dez'; 
+let varB = 'vinte'; 
+let varC = 'trinta'; 
+
+[varA, varB, varC] = [varB, varC, varA];
+
+console.log(varA, varB, varC);
