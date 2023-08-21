2984cf6a482ef78b58ed81aec3ef644b55e8422b
Gabriel.s <gabrielsantosayres@gmail.com>
2023-08-21 17:10:50 -0300

    Array avançado

---
+++ ex15/assets/js/script.js
@@ -0,0 +1,13 @@
+/*const nomes = ['Eduardo','Maria','Joana', 'Matheus','jhon','Rodrigo'];
+//delete nomes[2];
+const altera = nomes.splice(nomes.length ,0, "llllll", "kkkkkk", 'kjkfhfjfjf');
+
+console.log(nomes, altera);
+*/
+
+const a1 = [0,1,2,3];
+const a2 = [4,5,6,7];
+const a3 = a1.concat(a2);
+const a4 = [...a1,...a2, "array combinado","array combinado2"];
+
+console.log(a4.length);
---
+++ ex16/assets/js/script.js
@@ -0,0 +1,4 @@
+const a1 = [1,2,3,5,6,3,3,54,6,7,545,3,2,5,33,10,20,300, "um", "dois"];
+//const a2 = a1.filter(valor => valor%2 === 0);
+const a2 = a1.filter(valor => valor === "um");
+console.log(a2);
