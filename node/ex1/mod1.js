5e4b405ef9a763c243c06fcc48577d37124e0c53
Gabriel.s <gabrielsantosayres@gmail.com>
2023-09-01 16:53:53 -0300

    Import - Export

---
+++ node/ex1/mod1.js
@@ -0,0 +1,10 @@
+const nome = "rafael";
+const sobrenome = "albrecht";
+
+const falaNome = () => {
+    console.log(nome, sobrenome);
+};
+
+exports.nome = nome;
+exports.sobrenome = sobrenome;
+exports.falaNome = falaNome;
